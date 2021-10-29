1d7e65f0234461c90f5779120e85703620b1c51b
Nicholas McGee <nicholas.r.mcgee@gmail.com>
2021-10-28 21:09:50 -0600

    Added cache system to downloadAudio.py. Integrated into bot.py

--- bot.py
+++ bot.py
@@ -6,30 +6,39 @@ import downloadAudio
 
 
 class MyClient(discord.Client):
-    voiceClient = None
+    def __init__(self) -> None:
+        self.audioManager = downloadAudio.getAudio()
+        self.voiceClient = None
 
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
+        # Can't be the bot itself, or we'll get stuck in a loop
         if message.author == client.user:
             return
 
+        # User requesting must in a voice channel
         if message.author.voice is None:
             await message.channel.send('Must be in voice channel to request a song.')
 
         if message.content.startswith('!play '):
-            print('Downloading', message.content[6:])
+            results = self.audioManager.retrieveFile(message.content[6:])
+
+            if not results[0]:
+                await message.channel.send('Failed to retrieve song.')
+                return
+
+            print('Playing', results[1])
 
             if self.voiceClient is None:
                 print('Connecting to', message.author.voice.channel)
                 self.voiceClient = await message.author.voice.channel.connect()
 
-            downloadAudio.download(message.content[6:])
             self.voiceClient.play(discord.FFmpegPCMAudio(
-                source='music/YEAH_BOI.mp3'))
+                source=results[2]))
             while self.voiceClient.is_playing():
-                sleep(.1)
+                sleep(1)
             await self.voiceClient.disconnect()
 
 
--- downloadAudio.py
+++ downloadAudio.py
@@ -1,84 +1,120 @@
+from pytube.__main__ import YouTube
 import pytube.request
 from pytube import Search
 from pytube import Stream
 import re
 import sys
+import os
 
 # Compile regex pattern beforehand, speeds up time if ran a lot.
 pattern = re.compile(r'(?<!^)(?=[A-Z])')
 # Set downloads in 0.25MB chunks.
-pytube.request.default_range_size = 250000
+pytube.request.default_range_size = 500000
 
 
-def cleanName(title: str) -> str:
-    '''
-    Removes unwanted characters from YouTube video title to invalid filename on filesystem.
-    Source: https://stackoverflow.com/a/46801075/4364154
+class getAudio:
+    def __init__(self) -> None:
+        self.title = None
+        self.fileSize = None
+        self.currFiles = set(os.listdir('./music'))
 
-    Paramaters:
-    title (str): The name of the YouTube video
+    def cleanName(self, title: str) -> str:
+        '''
+        Removes unwanted characters from YouTube video title to invalid filename on filesystem.
+        Source: https://stackoverflow.com/a/46801075/4364154
 
-    Returns:
-    str: A cleaned up version of the title for easier storage.
-    '''
-    title = str(title).strip().replace(' ', '_')
-    return re.sub(r'(?u)[^-\w.]', '', title)
+        Parameters:
+        title (str): The name of the YouTube video
 
+        Returns:
+        str: A cleaned up version of the title for easier storage.
+        '''
+        title = str(title).strip().replace(' ', '_')
+        return re.sub(r'(?u)[^-\w.]', '', title)
 
-def _on_progress_callback(chunk: Stream, file_handle: bytes, bytes_remaining: int) -> None:
-    '''
-    In progress callback function from downloading a video or audio file. Called at every request.
-    Request size set by pytube.request.default_range_size.
+    def _on_progress_callback(self, chunk: Stream, file_handle: bytes, bytes_remaining: int) -> None:
+        '''
+        In progress callback function from downloading a video or audio file. Called at every request.
+        Request size set by pytube.request.default_range_size.
 
-    Paramaters:
-    chunk (Stream): Segment of media file binary data, not yet written to disk.
-    file_handle (Bytes): The file handle where the media is being written to.
-    bytes_remaining (int): How many bytes left to be downloaded.
+        Parameters:
+        chunk (Stream): Segment of media file binary data, not yet written to disk.
+        file_handle (Bytes): The file handle where the media is being written to.
+        bytes_remaining (int): How many bytes left to be downloaded.
 
-    Returns:
-    None
-    '''
-    print('MB Remaining:', bytes_remaining / 1000, 'MB')
+        Returns:
+        None
+        '''
+        print('{0} progress: {1:.2f}%'.format(self.title,
+              100.0 - (bytes_remaining / self.fileSize) * 100.0))
 
+    def _on_complete_callback(self, stream: Stream, file_path: str) -> None:
+        '''
+        Completion callback function from downloading a video or audio file, listing both the YouTube 
+        video title and the filepath the file was saved to.
 
-def _on_complete_callback(stream: Stream, file_path: str) -> None:
-    '''
-    Completion callback function from downloading a video or audio file, listing both the YouTube 
-    video title and the filepath the file was saved to.
+        Parameters:
+        stream (Stream): The Stream object of the YouTube video.
+        file_path (str): A string of the file path where the file was saved.
 
-    Paramaters:
-    stream (Stream): The Stream object of the YouTube video.
-    file_path (str): A string of the file path where the file was saved.
+        Returns:
+        None
+        '''
+        print('Downloaded \"{0}\" to {1}'.format(stream.title, file_path))
 
-    Returns:
-    None
-    '''
-    print('Downloaded \"{0}\" to {1}'.format(stream.title, file_path))
+    def download(self, audio: YouTube, title: str) -> None:
+        '''
+        Download an audio file from YouTube. Saves audio file as .mp3 to the directory 
+        ./music in the current directory.
 
+        Parameters:
+        title (str): The title of the video which will be saved.
+        audio (YouTube): A YouTube object of the queried title.
 
-def download(searchQuery) -> None:
-    '''
-    Download an audio file from YouTube. Can search by either YouTube URL, or by search text.
-    If search text is used, the first search result will be downloaded. Saves audio file as
-    .mp3 to the directory ./music in the current directory.
+        Return:
+        None
+        '''
+        audio.register_on_progress_callback(self._on_progress_callback)
+        audio.register_on_complete_callback(self._on_complete_callback)
+        audio = audio.streams.get_audio_only()
+        self.fileSize = audio.filesize
+        audio.download(filename=title, output_path='./music')
 
-    Paramaters:
-    searchQuery (str): Either a string search query or YouTube URL link.
+    def retrieveFile(self, searchQuery: str) -> list:
+        '''
+        Check if a YouTube video exists and has already been downloaded. If not downloaded, 
+        will request to download. Can search by either YouTube URL, or by search text. If 
+        search text is used, the first search result will be downloaded.
 
-    Return:
-    None
-    '''
-    audio = Search(searchQuery)
-    audio = audio.results[0]
-    audio.register_on_progress_callback(_on_progress_callback)
-    audio.register_on_complete_callback(_on_complete_callback)
-    audio = audio.streams.get_audio_only()
-    title = cleanName(audio.title) + '.mp3'
-    audio.download(filename=title, output_path='./music')
+        Parameters:
+        searchQuery (str): 
+
+        Return:
+        list(bool, str, str): Return if file retrieved, the YouTube title name, and the filepath.
+        '''
+        try:
+            audio = Search(searchQuery)
+            audio = audio.results[0]
+            self.title = self.cleanName(audio.title) + '.mp3'
+        except:
+            print('File not found.')
+            return (False, 'None', 'None')
+
+        if self.title not in self.currFiles:
+            self.download(audio, self.title)
+            self.currFiles.add(self.title)
+        else:
+            print(self.title, 'already downloaded!')
+        
+        return (True, audio.title, './music' + self.title)
 
 
 def main(searchQuery):
-    download(searchQuery)
+    musicQueue = ['big iron', 'kanye west waves', 'marty robbins el paso']
+
+    audioManager = getAudio()
+    for music in musicQueue:
+        print(audioManager.retrieveFile(music))
 
 
 if __name__ == '__main__':
