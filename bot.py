5cc0431546ca1a7148d2a528f1a0622e8d501d12
Nicholas McGee <nicholas.r.mcgee@gmail.com>
2021-10-29 22:53:04 -0600

    Rewrote bot. Queue system not optimal and buggy still.

--- bot.py
+++ bot.py
@@ -1,45 +1,84 @@
+import asyncio
 from time import sleep
+import traceback
 import discord
-from discord.voice_client import VoiceClient
+from discord import member
+from discord.ext import tasks, commands
+from discord.ext.commands.core import after_invoke
 import config
 import getAudio
 
+bot = commands.Bot(command_prefix='!')
+audioManager = getAudio.getAudio()
+songQueue = list()
+playing = False
 
-class MyClient(discord.Client):
-    def __init__(self, *args, **kwargs) -> None:
-        self.audioManager = getAudio.getAudio()
-        self.voiceClient = None
-        super().__init__(*args, **kwargs)
+@bot.event
+async def on_ready():
+    print('{0.user} logged on!'.format(bot))
 
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+@bot.command(name='play')
+async def play(ctx, *, arg):
+    try:
+        if ctx.author.voice is None:
+            print('{0.author} is not in a voice channel'.format(ctx))
+            await ctx.send('You must be in a voice channel to run this commmand.')
+            return 
+        
+        voiceChannel = ctx.author.voice.channel
+        voiceClient = bot.voice_clients[0] if len(bot.voice_clients) > 0 else None
+        if voiceClient:
+            await voiceClient.move_to(voiceChannel)
+        else:
+            voiceClient = await voiceChannel.connect()
 
-    async def on_message(self, message):
-        # Can't be the bot itself, or we'll get stuck in a loop
-        if message.author == client.user:
+        audio = audioManager.retrieveFile(arg)
+        if not audio[0]:
+            print('Failed to retrieve song.')
+            await ctx.send('Failed to retrieve ' + arg)
             return
+        
+        songQueue.append((audio[1], audio[2]))
+        await playAudio(ctx, voiceClient)
 
-        # User requesting must in a voice channel
-        if message.author.voice is None:
-            await message.channel.send('Must be in voice channel to request a song.')
-
-        if message.content.startswith('!play '):
-            results = self.audioManager.retrieveFile(message.content[6:])
-
-            if not results[0]:
-                print('Failed to retrieve song.')
-                await message.channel.send('Failed to retrieve song.')
-                return
-
-            print('Playing', results[1])
-            await message.channel.send('Playing ' + results[1])
-
-            if self.voiceClient is None:
-                print('Connecting to', message.author.voice.channel)
-                self.voiceClient = await message.author.voice.channel.connect()
-
-            self.voiceClient.play(discord.FFmpegPCMAudio(source=results[2]))
+    except Exception:
+        traceback.print_exc()
 
 
-client = MyClient()
-client.run(config.token)
+async def playAudio(ctx, voiceClient):
+    global playing
+    if playing:
+        return
+
+    while len(songQueue) > 0:
+        song = songQueue.pop(0)
+        await ctx.send('Playing {0}'.format(song[0]))
+
+        playing = True
+        voiceClient.play(discord.FFmpegPCMAudio(song[1]))
+
+        while voiceClient.is_playing():
+            await asyncio.sleep(2)
+
+    playing = False
+    print('thread finished')
+
+
+@bot.command(name='queue')
+async def queue(ctx):
+    if len(songQueue) == 0:
+        await ctx.send('There are currently no songs in the queue.')
+    else:
+        songList = [song[0] for song in songQueue]
+        songList = '\n'.join(songList)
+        await ctx.send('Song queue:\n' + songList)
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member == bot.user and after.channel is not None:
+        print('Peepo connected to voice channel {0}'.format(after.channel.name))
+    elif member == bot.user and after.channel is None:
+        print('Peepo left voice channel {0}'.format(before.channel.name))
+
+bot.run(config.token)
