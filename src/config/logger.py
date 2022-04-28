a2d7e5c863e9dfeea75b905a5bddf94293c7abfd
Nick-McGee <nicholas.r.mcgee@gmail.com>
2022-04-27 22:35:26 -0600

    Bot 2.0

---
+++ src/config/logger.py
@@ -0,0 +1,31 @@
+# Source: https://stackoverflow.com/a/56944256
+import logging
+
+class CustomFormatter(logging.Formatter):
+    grey = '\x1b[38;20m'
+    green = '\u001b[32m'
+    yellow = '\x1b[33;20m'
+    red = '\x1b[31;20m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+    format = '[%(asctime)s] [%(levelname)-8s] [%(filename)s:%(funcName)s:%(lineno)d] %(message)s'
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(CustomFormatter())
+logger.addHandler(ch)
